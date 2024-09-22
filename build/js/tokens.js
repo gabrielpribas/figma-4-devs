9caf682a8e074e38015012d235d3dbabc1430b1b
Gabriel Ribas <gabrielribas@gmail.com>
2024-09-22 13:27:10 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,17 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 22 Sep 2024 16:08:32 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray300": "#606064",
+  "ColorGray500": "#27272a",
+  "ColorGray800": "#121214",
+  "ColorGray900": "#121214",
+  "ColorBlue800": "#1e40af",
+  "VariableCollectionWhite": "#ffffffff",
+  "VariableCollectionBlue500": "#172554ff",
+  "VariableCollectionGray800": "#27272aff"
+};
